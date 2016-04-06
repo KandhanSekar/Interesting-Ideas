86a9a1e6f1d51dc4c68f98b15953f84636dbd8d0
KandhanSekar <kandhan3694@gmail.com>
2016-04-06 01:02:38 -0700

    a

---
+++ implementing relational operator without relop.cpp
@@ -0,0 +1,16 @@
+// ConsoleApplication6.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	cout << "enter the two numbers";
+	cin >> a >> b;
+	cout << "the greater of the numbers is ";
+	cout << ((sqrt(a*a + b*b - 2 * a*b) + (a + b)) / 2);
+    return 0;
+}
+
